72b6d7492fc38a06fc52729035f273d35565923d
JasonKori <koriesta8@gmail.com>
2024-07-20 10:01:33 +0300

    Add functions.js

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+function getAdd(num1, num2) {
+    return num1 + num2
+    }
+    
+    function getSub (num1, num2) {
+    return num1 - num2
+    }
+    
+    function getDiv (num1, num2) {
+    return num1 / num2
+    }
+    
+    
+    function getMulti (num1 , num2) {
+        return num1 * num2
+    }
+    
+    function getModul (num1, num2) {
+        return num1 % num2
+    }
