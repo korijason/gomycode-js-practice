1e97cccc36f116060c1e38347f1f044c5e4d4ee1
Jason Kori <koriesta8@gmail.com>
2024-07-20 10:24:44 +0300

    Add arrays.js file

---
+++ js/arrays.js
@@ -0,0 +1,29 @@
+let numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers)
+//Add an element at the end of the array
+numbers.push(1)
+console.log(numbers)
+//Add two more elements at the end of the array
+numbers.push(3, 5)
+
+console.log(numbers)
+
+//2. Pop
+//Remove an element from the end of the array
+
+numbers.pop()
+console.log(numbers)
+
+//3. Shift
+//Remove the first element in an array
+
+numbers.shift()
+
+console.log(numbers)
+
+
+//4. Unshift
+//Ad an eleement to the beginnning of an array
+numbers.unshift(1)
+console.log(numbers)
