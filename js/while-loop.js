df766e4411d259346885b310db322c3b9f9f10ea
Jason Kori <koriesta8@gmail.com>
2024-07-20 12:59:08 +0300

    Update index.html file

---
+++ js/while-loop.js
@@ -0,0 +1,16 @@
+//Printing 1 to 10 using while loop
+let count = 1;
+while (count <= 10){
+
+
+console.log(count);
+count ++;
+}
+
+//A counter from 10 to 1
+
+let num = 10;
+while (num => 1) {
+    console.log(num);
+    count--;
+}
